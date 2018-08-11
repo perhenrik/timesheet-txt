2a4980970b665b627a5fab2a778b1fcda554e1e3
Per Henrik Johansen <perhenrik@pm.me>
2018-08-11 12:48:58 +0200

    implemented MakeSureArrayHasEnoughElements()

--- util/array.go
+++ util/array.go
@@ -1,9 +1,8 @@
 package util
 
 // MakeSureArrayHasEnoughElements adds empty elements to an array unil it has the expected number of elements
-func MakeSureArrayHasEnoughElements(array []string, numberOfElements int) []string {
-	if len(array) < numberOfElements {
-		array = append(array, "")
-	}
-	return array
+func MakeSureArrayHasEnoughElements(in []string, length int) (out []string) {
+	out = make([]string, length)
+	copy(out, in)
+	return
 }
